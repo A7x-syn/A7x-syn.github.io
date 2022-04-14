c4f81296f2323aecc1f0b53387be573cff3d59bb
dna <kiluas741@naver.com>
2022-04-14 13:46:54 +0900

    search

--- assets/tipuesearch/tipuesearch_set.js
+++ assets/tipuesearch/tipuesearch_set.js
@@ -78,32 +78,3 @@ var tipuesearch_string_15 = 'Searches related to';
 
 var startTimer = new Date().getTime();
 
-
-/* 검색바 추가 */
-const input = document.querySelector(".finder__input");
-const finder = document.querySelector(".finder");
-const form = document.querySelector("form");
-
-input.addEventListener("focus", () => {
-  finder.classList.add("active");
-});
-
-input.addEventListener("blur", () => {
-  if (input.value.length === 0) {
-    finder.classList.remove("active");
-  }
-});
-
-form.addEventListener("submit", (ev) => {
-  ev.preventDefault();
-  finder.classList.add("processing");
-  finder.classList.remove("active");
-  input.disabled = true;
-  setTimeout(() => {
-    finder.classList.remove("processing");
-    input.disabled = false;
-    if (input.value.length > 0) {
-      finder.classList.add("active");
-    }
-  }, 1000);
-});
